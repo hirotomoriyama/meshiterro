96a33c2e471004ca2b58a1732a0c8b58f6d2c76d
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-02-14 04:46:58 +0000

    [Finish]4-3:画像投稿機能を作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6c10c593d93f9b9c6f935c9f887dbc87cbae574f61af97d55eb7a887d725e057bb9cb1f29ea6cc7c5edfb3a137209a2309873f2da02f05784ca487c1fb3f66f0'
